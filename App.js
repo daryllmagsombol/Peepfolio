76bd7ba4fdd87b753a712febdb0263e7213ca314
Daryll Magsombol <darjosh@192.168.254.104>
2021-08-10 08:52:37 +0800

    first commit

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		marginVertical: '10%',
-		marginHorizontal: '10%',
+		marginHorizontal: '7%',
 		// alignItems: 'center',
 		// justifyContent: 'center',
 	},
--- assets/js/home/homescreen.js
+++ assets/js/home/homescreen.js
@@ -1,45 +1,98 @@
-import React from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
-import { Card, Text } from '@ui-kitten/components'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { ScrollView, StyleSheet, View, TouchableOpacity } from 'react-native'
+import { Card, Text, Button } from '@ui-kitten/components'
 
-const renderStockCards = () => {
-	let cards = []
+const Homescreen = () => {
+	const [stocks, setStocks] = useState([
+		{
+			symbol: 'MBT',
+			price: { amount: 49.5 },
+			percent_change: 10.01,
+		},
+		{
+			symbol: 'CEB',
+			price: { amount: 39.05 },
+			percent_change: 0,
+		},
+		{
+			symbol: 'LTG',
+			price: { amount: 18 },
+			percent_change: -160.99,
+		},
+	])
+	// useEffect(() => {
+	// 	axios.get('http://phisix-api4.appspot.com/stocks.json').then((result) => {
+	// 		let data = result.data.stock
 
-	for (let i = 0; i < 20; i++) {
-		let randomStatus = Math.random() < 0.5
-		cards.push(
-			<Card
-				style={styles.stock_port_card}
-				status={randomStatus ? 'success' : 'danger'}
-			>
-				<View style={styles.stock_port_detail}>
-					<Text>AAA - SAMPLE STOCK {i + 1}</Text>
-				</View>
-			</Card>
-		)
+	// 		setStocks(data)
+	// 	})
+	// })
+
+	const renderStockCards = () => {
+		console.log('result', stocks)
+		return stocks.map((stock) => {
+			let percent = stock.percent_change
+			return (
+				<Card key={stock.symbol} style={styles.stock_port_card}>
+					<View style={styles.stock_port_detail}>
+						<View>
+							<Text style={{ fontWeight: 'bold' }}>{stock.symbol}</Text>
+							<Text style={{ color: '#93979a' }}>{stock.price.amount}</Text>
+						</View>
+
+						<View style={{ justifyContent: 'center' }}>
+							<Text>100.00</Text>
+						</View>
+						<Button
+							style={{
+								transform: [{ scale: 0.7 }],
+								paddingHorizontal: 0,
+								paddingVertical: 0,
+								marginLeft: 'auto',
+								borderRadius: 30,
+								width: 110,
+								height: 20,
+							}}
+							status={
+								percent < 0 ? 'danger' : percent > 0 ? 'success' : 'basic'
+							}
+						>
+							<Text
+								style={{
+									fontWeight: 'bold',
+									color: stock.percent_change === 0 ? '#8e9bb3' : 'white',
+									fontSize: 18,
+								}}
+							>
+								{stock.percent_change}%
+							</Text>
+						</Button>
+					</View>
+				</Card>
+			)
+		})
 	}
 
-	return cards
+	return (
+		<View>
+			<Card style={styles.card_port}>
+				<Text style={styles.market_text}>Market Value</Text>
+				<Text>
+					<Text style={styles.currency_text}>₱</Text>
+					<Text style={styles.amount_text}>100000.00</Text>
+				</Text>
+				<Text style={styles.percent_text}>6.6%</Text>
+			</Card>
+			<ScrollView style={styles.stock_port}>{renderStockCards()}</ScrollView>
+		</View>
+	)
 }
-
-const Homescreen = () => (
-	<View>
-		<Card style={styles.card_port}>
-			<Text style={styles.market_text}>Market Value</Text>
-			<Text>
-				<Text style={styles.currency_text}>₱</Text>
-				<Text style={styles.amount_text}>100000.00</Text>
-			</Text>
-			<Text style={styles.percent_text}>6.6%</Text>
-		</Card>
-		<ScrollView style={styles.stock_port}>{renderStockCards()}</ScrollView>
-	</View>
-)
 
 const styles = StyleSheet.create({
 	card_port: {
-		marginTop: '15%',
-		paddingVertical: '5%',
+		marginTop: '10%',
+		paddingVertical: '4%',
 		backgroundColor: '#79B0FA',
 		shadowColor: '#000',
 		shadowOffset: {
@@ -53,33 +106,33 @@ const styles = StyleSheet.create({
 	},
 	market_text: {
 		color: 'white',
-		fontSize: 17,
+		fontSize: 14,
 	},
 
 	currency_text: {
-		fontSize: 34,
+		fontSize: 32,
 		color: 'white',
 	},
 	amount_text: {
-		fontSize: 43,
+		fontSize: 39,
 		color: 'white',
 	},
 	percent_text: {
-		marginTop: '6%',
+		marginTop: '3%',
 		color: 'white',
 	},
 
 	stock_port: {
-		marginTop: '10%',
+		marginTop: '5%',
 		marginBottom: '75%',
-		marginHorizontal: '2%',
+		marginHorizontal: '1%',
 	},
 	stock_port_card: {
-		marginVertical: '3%',
+		marginVertical: '2.1%',
 		shadowColor: '#000',
 		shadowOffset: {
-			width: 0,
-			height: 3,
+			width: 5,
+			height: 5,
 		},
 		shadowOpacity: 0.23,
 		shadowRadius: 2.62,
@@ -87,7 +140,10 @@ const styles = StyleSheet.create({
 		// height: '7%',
 	},
 	stock_port_detail: {
-		paddingVertical: '7%',
+		paddingVertical: '3%',
+		marginHorizontal: '-5%',
+		flexDirection: 'row',
+		// flexWrap: 'nowrap',
 	},
 })
 
